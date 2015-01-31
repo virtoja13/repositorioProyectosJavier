41f9da80d42f68e5764bdb34120ea58fe3717fde
Javier Virviescas <virtoja13@hotmail.com>
2015-01-31 08:27:57 -0500

    Segunda version del proyecto adicionando e ignorando mas archivos

---
+++ src/main/java/edu/uniandes/ecos/LAB1/app/App.java
@@ -0,0 +1,13 @@
+package edu.uniandes.ecos.LAB1.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
